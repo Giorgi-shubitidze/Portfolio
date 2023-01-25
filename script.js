92de23f9b08e690cd8deb365c6fc5f813eb82640
Giorgi Shubitidze <giorgishubitidze2066@gmail.com>
2023-01-25 03:08:23 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function mail() {
+    document.querySelector(".mail").classList.toggle("myMail")
+}
+
+function menuEl() {
+    document.querySelector(".menu").classList.toggle("show")
+}
+
+
+function videop1() {
+    var project = document.querySelector(".twitch").classList.toggle("twitchtog")
+    var name = document.querySelector(".pjname").classList.toggle("pjnametog")
+    var p = document.querySelector(".pj1").classList.toggle("pj1tog")
+    var btn = document.querySelector(".fbtn1").classList.toggle("btntog")
+}
+
+function videop2() {
+    var project = document.querySelector(".musicApp").classList.toggle("musicAPPtog")
+    var name = document.querySelector(".pjname").classList.toggle("pjnametog")
+    var p = document.querySelector(".pj2").classList.toggle("pj2tog")
+    var btn = document.querySelector(".fbtn2").classList.toggle("btntog")
+}
+
+function videop3() {
+    var project = document.querySelector(".singIn").classList.toggle("signIntog")
+    var name = document.querySelector(".pjname").classList.toggle("pjnametog")
+    var p = document.querySelector(".pj3").classList.toggle("pj3tog")
+    var btn = document.querySelector(".fbtn3").classList.toggle("btntog")
+}
+
